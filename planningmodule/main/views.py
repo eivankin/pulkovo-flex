5bdf4459b83a627f91fab6334f1f20d65963e525
Eugen <eug-potato@yandex.ru>
2020-08-15 14:56:01 +0300

    fix view

--- planningmodule/main/views.py
+++ planningmodule/main/views.py
@@ -21,7 +21,8 @@ class MyModelAdmin(admin.ModelAdmin):
             errors = self.save_data(request.FILES['file'])
             if not errors:
                 messages.info(request, 'Объекты успешно импортированы')
-            messages.error(request, f'Не удалось импортировать объекты: {", ".join(errors)}')
+            else:
+                messages.error(request, f'Не удалось импортировать объекты: {", ".join(errors)}')
         form = ImportForm()
         context = dict(self.admin_site.each_context(request), form=form)
         return render(request, 'main/import.html', context)
